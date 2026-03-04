ebe27133fdaf4895afe9912a81653121fabc75c4
Songju Kim <5s0n9@naver.com>
2026-03-04 23:55:32 +0900

    #1 Edit Text

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,8 @@
 <template>
-  <h1>hello world</h1>
+  <h1>hello world hello Git</h1>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
 }
