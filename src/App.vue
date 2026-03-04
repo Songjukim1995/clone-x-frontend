d63d4508ff1817c409cf07d2baedcd7b3f15dac7
Songjukim <songjukim@Songjukimui-MacBookPro.local>
2026-03-04 23:25:39 +0900

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>hello world</h1>
 </template>
 
 <script>
@@ -8,19 +7,8 @@ import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
